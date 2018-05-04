96d961a59190426497b464baa89c6114b036a67c
jyang <1107565856@qq.com>
2018-05-04 15:16:43 +0800

    把集群改成了桥接模式下，三台虚拟机，每台两个redis

--- redis/src/test/java/cn/jyuyang/jedis/JedisClusterTest.java
+++ redis/src/test/java/cn/jyuyang/jedis/JedisClusterTest.java
@@ -11,35 +11,35 @@ import redis.clients.jedis.JedisCluster;
 
 import java.util.HashSet;
 import java.util.Set;
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath:spring-redis-cluster.xml")
+/*@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring-redis-cluster.xml")*/
 public class JedisClusterTest {
 
-    @Autowired
-    private JedisCluster jedisCluster;
+//    @Autowired
+//    private JedisCluster jedisCluster;
     @Test
     public void save(){
-       /* GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
+       GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
         poolConfig.setMaxTotal(10);
         poolConfig.setMinIdle(5);
 
         Set<HostAndPort> hostAndPortSet = new HashSet<HostAndPort>();
 
-        hostAndPortSet.add(new HostAndPort("192.168.137.169",6001));
-        hostAndPortSet.add(new HostAndPort("192.168.137.169",6002));
-        hostAndPortSet.add(new HostAndPort("192.168.137.169",6003));
-        hostAndPortSet.add(new HostAndPort("192.168.137.169",6004));
-        hostAndPortSet.add(new HostAndPort("192.168.137.169",6005));
-        hostAndPortSet.add(new HostAndPort("192.168.137.169",6006));
+        hostAndPortSet.add(new HostAndPort("192.168.1.195",6004));
+        hostAndPortSet.add(new HostAndPort("192.168.1.195",6003));
+        hostAndPortSet.add(new HostAndPort("192.168.1.196",6005));
+        hostAndPortSet.add(new HostAndPort("192.168.1.196",6006));
+        hostAndPortSet.add(new HostAndPort("192.168.1.197",6007));
+        hostAndPortSet.add(new HostAndPort("192.168.1.197",6008));
         JedisCluster jedisCluster = new JedisCluster(hostAndPortSet,poolConfig);
         jedisCluster.set("name","tom");
-        System.out.println(jedisCluster.get("name"));*/
+        System.out.println(jedisCluster.get("name"));
 
     }
 
-    @Test
+  /*  @Test
     public void getName(){
         System.out.println(jedisCluster.get("name"));
     }
-
+*/
 }
