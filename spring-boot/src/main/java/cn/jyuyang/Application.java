c0892a97d5c2614007c562ddf364de78d13b28ea
jyang <1107565856@qq.com>
2018-05-09 11:34:56 +0800

    spring-boot构建项目初试

---
+++ spring-boot/src/main/java/cn/jyuyang/Application.java
@@ -0,0 +1,20 @@
+package cn.jyuyang;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Application {
+
+	public static void main(String[] args) {
+
+	    SpringApplication.run(Application.class, args);
+	}
+
+}
